271857505bdc86d9e7529b160a911fee0a24f0a3
3APA3A-3AHO3A <93767742+3APA3A-3AHO3A@users.noreply.github.com>
2022-11-16 15:42:51 +0500

    fix code

--- build.py
+++ build.py
@@ -7,79 +7,24 @@ bot_logs = telebot.TeleBot(config.BOT_TOKEN_logs)
 
 def keyboard_pvp():
     keyboard = telebot.types.InlineKeyboardMarkup()
-    # for index, item in enumerate(config.pvp):
-    #         msg = "Герой: " + str(config.worksheet_build_pvp.cell(row=int(index) + 2, column=2).value) + \
-    #               "\nТалант: " + str(config.worksheet_build_pvp.cell(row=int(index) + 2, column=3).value) + \
-    #               "\nЭмблема: " + str(config.worksheet_build_pvp.cell(row=int(index) + 2, column=4).value) + \
-    #               "\nКвадрат: " + str(config.worksheet_build_pvp.cell(row=int(index) + 2, column=5).value) + \
-    #               "\nЧара: " + str(config.worksheet_build_pvp.cell(row=int(index) + 2, column=6).value) + \
-    #               "\nСозвездия: " + str(config.worksheet_build_pvp.cell(row=int(index) + 2, column=7).value)
-    #         img = open('Database/sborki/' + str(item) + '.jpg', 'rb')
-    #         media = telebot.types.InputMediaPhoto(img, caption=msg)
-    #         bot.edit_message_media(message_id=call.message.message_id, chat_id=call.message.chat.id,
-    #                                media=media, reply_markup=build.keyboard_pvp())
-    key_dinamo = telebot.types.InlineKeyboardButton(text='Динамо', callback_data='dinamo')
-    key_chezh_poryadok = telebot.types.InlineKeyboardButton(text='Чех (Порядок)',
-                                                            callback_data='chezh_poryadok')
-    key_chezh_chaos = telebot.types.InlineKeyboardButton(text='Чех (Хаос)', callback_data='chezh_chaos')
-    key_zmeya = telebot.types.InlineKeyboardButton(text='Змей', callback_data='zmeya')
-    key_baron = telebot.types.InlineKeyboardButton(text='Барон', callback_data='baron')
-    key_povelitel = telebot.types.InlineKeyboardButton(text='Повелитель', callback_data='povelitel')
-    key_mechnic = telebot.types.InlineKeyboardButton(text='Мечник', callback_data='mechnic')
-    key_bard = telebot.types.InlineKeyboardButton(text='Бард', callback_data='bard')
-    key_glaz = telebot.types.InlineKeyboardButton(text='Дурной глаз', callback_data='glaz')
-    key_hudozhka = telebot.types.InlineKeyboardButton(text='Художница', callback_data='hudozhka')
-    key_mari = telebot.types.InlineKeyboardButton(text='Мэри', callback_data='mari')
-    key_fox = telebot.types.InlineKeyboardButton(text='Лис', callback_data='fox')
-    key_frostee = telebot.types.InlineKeyboardButton(text='Фрости', callback_data='frostee')
-    key_fairy = telebot.types.InlineKeyboardButton(text='Фейри', callback_data='fairy')
-    key_okkult = telebot.types.InlineKeyboardButton(text='Оккультист', callback_data='okkult')
-    key_ronin = telebot.types.InlineKeyboardButton(text='Ронин', callback_data='ronin')
-    keyboard.add(
-        key_zmeya,
-        key_dinamo,
-        key_chezh_poryadok,
-        key_chezh_chaos,
-        key_mechnic,
-        key_fairy,
-        key_bard,
-        key_baron,
-        key_frostee,
-        key_glaz,
-        key_povelitel,
-        key_okkult,
-        key_hudozhka,
-        key_mari,
-        key_ronin,
-        key_fox,
-        row_width=3
-    )
+    list_keyboard = []
+    for index, item in enumerate(config.pvp):
+        name = config.worksheet_build_pvp.cell(row=int(index) + 2, column=2).value
+        key = config.worksheet_build_pvp.cell(row=int(index) + 2, column=1).value
+        list_keyboard.append(telebot.types.InlineKeyboardButton(text=name, callback_data=key))
+    keyboard.add(*list_keyboard, row_width=3)
     return keyboard
 
 
 def keyboard_bg():
     keyboard = telebot.types.InlineKeyboardMarkup()
-    key_sekach = telebot.types.InlineKeyboardButton(text='Секач', callback_data='sekach')
-    key_anub = telebot.types.InlineKeyboardButton(text='Анубис', callback_data='anub')
-    key_bard_bg = telebot.types.InlineKeyboardButton(text='Бард', callback_data='bard_bg')
-    key_draik = telebot.types.InlineKeyboardButton(text='Дрейк', callback_data='draik')
-    key_zefir = telebot.types.InlineKeyboardButton(text='Зефирик', callback_data='zefir')
-    key_minos = telebot.types.InlineKeyboardButton(text='Вождь Минотавров', callback_data='minos')
-    key_treant = telebot.types.InlineKeyboardButton(text='Треант', callback_data='treant')
-    key_fen = telebot.types.InlineKeyboardButton(text='Феникс', callback_data='fen')
-    key_major = telebot.types.InlineKeyboardButton(text='Мажор-сборка', callback_data='major')
-    keyboard.add(
-        key_sekach,
-        key_anub,
-        key_bard_bg,
-        key_draik,
-        key_zefir,
-        key_minos,
-        key_treant,
-        key_fen,
-        row_width=3
-    )
-    keyboard.add(key_major)
+    list_keyboard = []
+    for index, item in enumerate(config.bg):
+        name = config.worksheet_build_bg.cell(row=int(index) + 2, column=2).value
+        key = config.worksheet_build_bg.cell(row=int(index) + 2, column=1).value
+        list_keyboard.append(telebot.types.InlineKeyboardButton(text=name, callback_data=key))
+    keyboard.add(*list_keyboard, row_width=3)
+    keyboard.add(telebot.types.InlineKeyboardButton(text='Мажор-сборка', callback_data='major'))
     return keyboard
 
 
